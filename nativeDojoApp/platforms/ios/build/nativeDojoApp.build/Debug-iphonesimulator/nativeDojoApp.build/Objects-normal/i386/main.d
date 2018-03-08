b227ad70ec0c71678a90362bd99dc49fdd76e4d3
Tyler Blake <tyler.blake.louisville@gmail.com>
2018-03-07 19:23:20 -0500

    Initial commit of angular tab template project

---
+++ nativeDojoApp/platforms/ios/build/nativeDojoApp.build/Debug-iphonesimulator/nativeDojoApp.build/Objects-normal/i386/main.d
@@ -0,0 +1,11 @@
+dependencies: \
+  /Users/tyler/Documents/github/nativeDojo/nativeDojoApp/platforms/ios/nativeDojoApp/nativeDojoApp-Prefix.pch \
+  /Users/tyler/Documents/github/nativeDojo/nativeDojoApp/platforms/ios/internal/main.m \
+  /Users/tyler/Documents/github/nativeDojo/nativeDojoApp/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/tyler/Documents/github/nativeDojo/nativeDojoApp/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntimeInstrumentation.h \
+  /Users/tyler/Documents/github/nativeDojo/nativeDojoApp/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/tyler/Documents/github/nativeDojo/nativeDojoApp/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/tyler/Documents/github/nativeDojo/nativeDojoApp/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/tyler/Documents/github/nativeDojo/nativeDojoApp/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/tyler/Documents/github/nativeDojo/nativeDojoApp/platforms/ios/internal/TNSDebugging.h \
+  /Users/tyler/Documents/github/nativeDojo/nativeDojoApp/platforms/ios/internal/TKLiveSync/include/TKLiveSync.h
